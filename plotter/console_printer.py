18b1009ebdb7531873cd34a485965e4be586ece2
Tomás Yany <tomasyany@gmail.com>
2015-04-29 21:50:03 -0300

    Add welcome message method.

--- plotter/console_printer.py
+++ plotter/console_printer.py
@@ -3,10 +3,24 @@
 class ConsolePrinter(object):
 
     @staticmethod
-    def console(time, event):
+    def step(time, event):
         """Prints directly on the console."""
         
         print ("--------------------------------\n")
         print ("Time = "+str(time)+" - "+ event)
 
+    @staticmethod
+    def welcome():
+        """Prints welcome message."""
+        
+        space = "                       "
+        lines = "----------------------------------"+\
+        "------------------------------------------------" 
 
+        print ('\n\n'+lines)
+
+        print (space+"Welcome to the TRUCK SIMULATOR\n"+
+                "A simulation sofware by Oscar Flores, Manuel Pérez, "+ 
+                "Tomás Yany and Roberto Zúñiga.")
+
+        print (lines+'\n\n')
