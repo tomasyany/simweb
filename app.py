8df8deb7cd7200e3771077a3350c2585fc649bad
rocamadour <rbzuniga@gmail.com>
2015-05-01 20:57:16 -0300

    Versión funcional sin inventario. Faltan comentarios (se vienen pronto jajaja)

--- app.py
+++ app.py
@@ -3,10 +3,10 @@
 import simpy
 import numpy
 
-from processes.truck import Truck
-from processes.workshop import Workshop
-from processes.inventory import Inventory
-from plotter.console_printer import ConsolePrinter as printer
+from truck import Truck
+from workshop import Workshop
+from random_generator import RandomTime
+from fleet import Fleet
 
 
 # Constants
@@ -15,17 +15,27 @@ RANDOM_SEED = 42
 TRUCKS_AMOUNT = 20 # Total number of trucks (in use, at workshop or standing-by)
 TRUCKS_USE = 15 # Required number of trucks in use at the same time
 
+WORKSHOP_CAPACITY = 4
+
 COMPONENTS = 5 # Number of components
-INITIAL_STOCK = 20 # Number of initial stock per component in the inventory
+LIFETIME_MEAN = 20
+REPAIRTIME_MEAN = 5
+REPLACEMENTTIME_MEAN = 3
 
-WORKSHOP_CAPACITY = 50 # Amount of parking spot at the workshop
-
-SIM_TIME = 20 # Simulation horizon
+SIMULATION_HORIZON = 1000
 
 def main():
     """Main function to be runned."""
 
     env = simpy.Environment()
+    # lifetime, repair time and inventory replacement time distributions for each distribution
+    c = []
+    for i in range(COMPONENTS):
+        c.append([RandomTime('exponential',LIFETIME_MEAN), RandomTime('exponential',REPAIRTIME_MEAN), RandomTime('exponential',REPLACEMENTTIME_MEAN)])
+    fleet = Fleet(1,c,TRUCKS_AMOUNT,TRUCKS_USE,env)
+    for i in range(WORKSHOP_CAPACITY):
+        w = Workshop(env)
+    env.run(until=SIMULATION_HORIZON)
 
 if __name__ == "__main__":
     main()
---
+++ random_generator.py
@@ -0,0 +1,30 @@
+from numpy import random
+
+class RandomTime(object):
+    # This class is used to generate random numbers following a defined distribution
+    def __init__(self,distribution,*parameters):
+        self.dist = distribution
+        self.params = parameters
+    def getInstance(self):
+        dist = self.dist
+        p = self.params
+        if dist == 'exponential':
+            return random.exponential(p[0])
+        elif dist == 'normal':
+            return random.normal(p[0],p[1])
+        elif dist == 'uniform':
+            return random.uniform(p[0],p[1])
+        elif dist == 'poisson':
+            return random.poisson(p[0])
+        elif dist == 'binomial':
+            return random.binomial(p[0],p[1])
+        elif dist == 'geometric':
+            return random.geometric(p[0])
+        elif dist == 'weibull':
+            return random.weibull(p[0])
+        elif dist == 'gamma':
+            return random.gamma(p[0],p[1])
+        elif dist == 'beta':
+            return random.beta(p[0],p[1])
+        elif dist == 'lognormal':
+            return random.lognormal(p[0],p[1])
